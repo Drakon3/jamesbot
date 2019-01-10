33797ee658cd4505d3d6381ae534baf730552c38
James Harvey <jhar8856@shared.sydney.edu.au>
2019-01-10 15:47:45 +1100

    removed unecesarry functions

--- jamesbot.py
+++ jamesbot.py
@@ -1,26 +1,16 @@
 from flask import Flask, request
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
 
-@app.route('/ncss')
-def hello_ncss():
-    return 'Yay!'
 
 @app.route('/greet', methods=['GET', 'POST'])
 def greet_person():
     name = request.values.get('text')
     return f'Greetings {name}!'
 
-@app.route('/weather')
-def weather():
-    temp = int(request.values.get('temp'))
-    if temp > 30:
-        return "It's so hot!"
-    else:
-        return f"The temprature is {temp}"
+"""@app.route('/', methods = ['GET', 'POST'])
+def :"""
+
 
 if __name__ == '__main__':
     app.run()
