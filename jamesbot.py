348915778d00074f72076fe3338e0a21939d09be
James Harvey <jhar8856@SHARED.SYDNEY.EDU.AU>
2019-01-10 12:25:33 +1100

    removed all but one function

--- jamesbot.py
+++ jamesbot.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello, World!'
 
-@app.route('/ncss')
+"""@app.route('/ncss')
 def hello_ncss():
     return 'Yay!'
 
@@ -21,7 +21,7 @@ def weather():
         return "It's so hot!"
     else:
         return f"The temprature is {temp}"
-
+"""
 if __name__ == '__main__':
     app.run()
 
